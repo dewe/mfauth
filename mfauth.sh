59b02b9dae88d7f5f214656106c58587a472b69b
Johan Dewe <johan@dewe.net>
2019-01-29 13:14:08 +0100

    Make AWS_ROLE_ARN mandatory

--- mfauth.sh
+++ mfauth.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-# Examples:
-#
-# Auth with default identity
-# mfauth "My 1password item" 
-#
-# Assume role
-# mfauth "My 1password item" "arn:aws:iam::123456789012:role/MyFunkyRole"
-
 set -e
+
+# positional args array
+args=( )
+
+# parse args
+while (( "$#" )); do
+  case "$1" in
+    -d|--duration)
+      AWS_MFA_DURATION=$2
+      shift 2
+      ;;
+    --) # end argument parsing
+      shift
+      break
+      ;;
+    -*|--*=) # unsupported flags
+      echo Error: Unsupported argument $1 >&2
+      usage
+      exit 1
+      ;;
+    *) # preserve positional arguments
+      args+=( "$1" )
+      shift
+      ;;
+  esac
+done
+
+# set positional arguments in their proper place
+set -- "${args[@]}"
 
 # get arguments, or default values from environment variables
 OP_ITEM_NAME=${1:-$OP_ITEM_NAME}
 AWS_ROLE_ARN=${2:-$AWS_ROLE_ARN}
-
-# default duration value in seconds
 AWS_MFA_DURATION=${AWS_MFA_DURATION:-3600}
 
 main() {
@@ -23,11 +42,12 @@ main() {
     verify_dependency aws
 
     ensure_var "$OP_ITEM_NAME" "Missing 1Password item name as first argument or \$OP_ITEM_NAME"
+    ensure_var "$AWS_ROLE_ARN" "Missing 1Password item name as second argument or \$AWS_ROLE_ARN"
     ensure_var "$OP_SUBDOMAIN" "Missing 1Password subdomain as \$OP_SUBDOMAIN"
 
     assume_role=$(get_role_and_duration)
     mfa=$(get_mfa "$OP_SUBDOMAIN" "$OP_ITEM_NAME")
-    awsmfa $duration --token-code $mfa $assume_role
+    awsmfa --token-code $mfa $assume_role
 }
 
 get_mfa() {
@@ -36,7 +56,7 @@ get_mfa() {
 
 get_role_and_duration() {
     if [ -n "$AWS_ROLE_ARN" ]; then
-        echo -d $AWS_MFA_DURATION $AWS_ROLE_ARN
+        echo --duration $AWS_MFA_DURATION $AWS_ROLE_ARN
     fi
 }
 
@@ -63,4 +83,23 @@ abort() {
     exit 1
 }
 
-main
+usage() {
+      echo 
+      echo "Usage: mfauth [OP_ITEM_NAME] [AWS_ROLE_ARN] -d [AWS_MFA_DURATION]"
+      echo
+      echo "positional arguments:"
+      echo "  OP_ITEM_NAME     Name or uuid of the 1Password item that holds the"
+      echo "                   two factor authentication code. Default value from"
+      echo "                   OP_ITEM_NAME environment variable, if set."
+      echo "  AWS_ROLE_ARN     ARN for the AWS IAM role to assume. Default value"
+      echo "                   from AWS_ROLE_ARN environment variable, if set."
+      echo
+      echo "optional arguments:"
+      echo "  -d DURATION, --duration DURATION"
+      echo "                   The number of seconds for the temporary credentials"
+      echo "                   to be valid for. Default 1 hour. Max 1 hour when"
+      echo "                   assuming a role."
+      echo ""
+}
+
+main "$@"; exit
